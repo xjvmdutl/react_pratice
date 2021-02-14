84cf0921b57803899c12dea9997bde03d653df08
xjvmdutl <xjvmdutl@gmail.com>
2021-02-14 22:29:39 +0900

    ch03

---
+++ ch03/hello-react/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+import MyComponent from './MyComponent';
+
+// 컴포넌트 선언
+//방법 2가지 1. 함수형 2. class형(차이점 : 클레스형 컴포넌트인 경우 state 기능및 라이프 사이클 기능 사용 가능, 임의 메소드 정의 가능)
+//ESP 6 이후부터 자바스크립트에서 클레스 선언 가능 이전에는 prototype으로 선언
+//class형 컴포넌트 선언
+//함수형 컴포넌트 장점 : 선언에 용이함(클래스형보다 쉬움), 메모리 자원또한 덜 사용(프로젝트 빌드 베포시에도 결과물 크기가 작지만 별 차이 없음)
+//단점 state와 라이프사이클 API 사용이 불가능 => 16.8버전인 Hooks기능 도입후 해결
+
+
+// class  App extends Component{
+//   render(){
+//     const name = 'react';
+//     return <div className='react'>{name}</div>
+//   }
+// }
+
+// export default App;
+
+//모듈 불러오기
+//props 값 지정하기 , name 속성값을 지정해서 줄수 있다.
+// const App = () =>{
+//   return <MyComponent name='React'/>;
+// }
+
+// export default App;
+//default props = props 값을 따로 지정하지 않았을떄 보여주는 기본값
+// const App = () =>{
+//   return <MyComponent/>;
+// }
+
+// export default App;
+
+//태그 사이의 내용을 보여주는 children
+// 컴포넌트 태그 사이의 내용을 보여주는 props가존재 = children
+//
+const App = () =>{
+  return <MyComponent>리액트</MyComponent>;
+}
+
+export default App;
---
+++ ch03/hello-react/src/MyComponent.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';//컴포넌트의
+
+//일반 함수의 경우 자신이 종속된 객체를 this로 가리키며, 화살표 함수는 자신이 종속된 인스턴스를 가리킨다.
+function twice(value) {
+    return value*2;
+}
+const triple = (value) => value *3;
+//같은 의미
+//rsc를 입력하면 컴포넌트 코드를 쉽게 생성가능
+
+
+// const MyComponent = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+// };
+// //다른 파일에서 이파일을 import 할떄 MyComponent 클래스를 불러오도록 설정하는 코드
+// export default MyComponent;
+
+
+//props = properties 를 줄인 표현으로 컴포넌트 속성을 설정할 떄 사용하는 요소
+//해당 컴포넌트를 불러와 사용하는 부모 컴포넌트에서 설정 가능
+//props 값은 컴포넌트 함수의 파라미터로 받아 와서 사용할수 있다.
+// const MyComponent = props => {
+//     return (
+//         <div>
+//             안녕하세요 제 이름은 {props.name}입니다.
+//             {/*
+//                 children 값을 출력 하기 위해서는 위와같이 사용하여야 한다.
+//             */}
+//             <br />
+//             children값은 {props.children}입니다.
+//         </div>
+//     );
+// };
+//비구조화 할당 문법을 통해 props 내부값 추출
+//객체에서 값을 추출하는 문법이면 구조분해 문법이라고도 부른다.
+// 함수의 파라미터 부분에서도  사용 가능.
+// const MyComponent = props => {
+//     const { name, children} = props;
+//     return (
+//         <div>
+//             안녕하세요 제 이름은 {name}입니다.
+//             <br />
+//             children값은 {children}입니다.
+//         </div>
+//     );
+// };
+//함수의 파라미터가 객체라면 그값을 바로 비구조화하여 사용하는것
+const MyComponent = ({name,children}) => {
+    return (
+        <div>
+            안녕하세요 제 이름은 {name}입니다.
+             <br />
+             children값은 {children}입니다.
+        </div>
+    );
+}
+
+//default props
+MyComponent.defaultProps = {
+    name : '기본 이름' 
+};
+export default MyComponent;
